b58983fbbb3f04819d11e2031769f4c8d99fc0f9
Rutvi Tilala <37977465+rutvi21@users.noreply.github.com>
2020-06-15 23:47:52 +0530

    Add files via upload

---
+++ scale.py
@@ -0,0 +1,27 @@
+import imageio
+import imgaug as ia
+import imgaug.augmenters as iaa
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib
+import os
+import cv2
+#globbing utility.
+import glob
+
+path = "C:\Users\lenovo\PycharmProjects\face_classification\train"
+seq=iaa.Affine(rotate=(-50, 30))
+for folder in os.listdir(path):
+    i = 0
+    for fname in os.listdir(path + '\\' + folder):
+        img = imageio.imread(path + '\\' + folder + '\\' + fname)
+        print('Original:')
+        ia.imshow(img)
+        img_aug = seq.augment_image(img)
+        print('Augmented:')
+        ia.imshow(img_aug)
+
+        imageio.imwrite(os.path.join(path, path + '\\' + folder + '\\' + folder + "%06d.png" % (i,)), img_aug)
+        i += 1
